cb586d73209b1d529843c40baed8ffeb332283f6
anOtherAnalyse <23277378+anOtherAnalyse@users.noreply.github.com>
2020-12-17 15:24:35 +0100

    Adding book support for stare command

--- src/main/java/family_fun_pack/commands/PeekCommand.java
+++ src/main/java/family_fun_pack/commands/PeekCommand.java
@@ -2,8 +2,12 @@ package family_fun_pack.commands;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiScreenBook;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemShulkerBox;
+import net.minecraft.item.ItemWritableBook;
+import net.minecraft.item.ItemWrittenBook;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.nbt.NBTTagCompound;
@@ -15,7 +19,7 @@ import net.minecraftforge.fml.relauncher.Side;
 
 import family_fun_pack.gui.interfaces.PreviewGui;
 
-/* Shulker peek, but for shulker dropped on the floor or hold by other entities */
+/* Shulker/book peek, but for shulker/book dropped on the floor or hold by other entities */
 
 @SideOnly(Side.CLIENT)
 public class PeekCommand extends Command {
@@ -34,6 +38,8 @@ public class PeekCommand extends Command {
   public String execute(String[] args) {
     Minecraft mc = Minecraft.getMinecraft();
 
+    boolean book = (args.length > 1 && args[1].equals("book")) ? true : false;
+
     ItemStack stack = null;
     for(Entity entity : mc.world.<Entity>getEntitiesWithinAABB(Entity.class, mc.player.getEntityBoundingBox().grow(12.0D, 4.0D, 12.0D))) {
 
@@ -41,7 +47,7 @@ public class PeekCommand extends Command {
 
       // Search through entities metadata
       for(EntityDataManager.DataEntry<?> entry : entity.getDataManager().getAll()) {
-        if(entry.getValue() instanceof ItemStack && ((ItemStack) entry.getValue()).getItem() instanceof ItemShulkerBox) {
+        if(entry.getValue() instanceof ItemStack && ((book && (((ItemStack) entry.getValue()).getItem() instanceof ItemWritableBook || ((ItemStack) entry.getValue()).getItem() instanceof ItemWrittenBook)) || (!book && ((ItemStack) entry.getValue()).getItem() instanceof ItemShulkerBox))) {
           stack = (ItemStack) entry.getValue();
           break;
         }
@@ -50,7 +56,7 @@ public class PeekCommand extends Command {
 
       // Search through entity equipment
       for(ItemStack item : entity.getEquipmentAndArmor()) {
-        if(item.getItem() instanceof ItemShulkerBox) {
+        if((!book && item.getItem() instanceof ItemShulkerBox) || (book && (item.getItem() instanceof ItemWritableBook || item.getItem() instanceof ItemWrittenBook))) {
           stack = item;
           break;
         }
@@ -58,20 +64,33 @@ public class PeekCommand extends Command {
       if(stack != null) break;
     }
 
-    if(stack == null) return "No shulker item close to you";
+    if(stack == null) return "No " + (book ? "book" : "shulker") + " item close to you";
 
-    NBTTagCompound tag = stack.getTagCompound();
-    if(tag != null && tag.hasKey("BlockEntityTag") && tag.getTagId("BlockEntityTag") == 10) {
-      NBTTagCompound blockTag = tag.getCompoundTag("BlockEntityTag");
-      if(blockTag.hasKey("Items") && blockTag.getTagId("Items") == 9) {
-        this.screen = new PreviewGui(blockTag.getTagList("Items", 10), false);
-        MinecraftForge.EVENT_BUS.register(this);
-        return null;
+    if(book) {
+
+      if(stack.getItem() instanceof ItemWritableBook) {
+        stack = stack.copy();
+        stack.getTagCompound().setString("title", "Writable book");
+        stack.getTagCompound().setString("author", "No author");
       }
-    }
 
-    if(tag == null) return "Shulker is empty, no tag";
-    return "Shulker does not contain items, no BlockEntityTag tag, tag is " + tag.toString();
+      this.screen = new GuiScreenBook(mc.player, stack, false);
+      MinecraftForge.EVENT_BUS.register(this);
+    } else {
+      NBTTagCompound tag = stack.getTagCompound();
+      if(tag != null && tag.hasKey("BlockEntityTag") && tag.getTagId("BlockEntityTag") == 10) {
+        NBTTagCompound blockTag = tag.getCompoundTag("BlockEntityTag");
+        if(blockTag.hasKey("Items") && blockTag.getTagId("Items") == 9) {
+          this.screen = new PreviewGui(blockTag.getTagList("Items", 10), false);
+          MinecraftForge.EVENT_BUS.register(this);
+          return null;
+        }
+      }
+
+      if(tag == null) return "Shulker is empty, no tag";
+      return "Shulker does not contain items, no BlockEntityTag tag, tag is " + tag.toString();
+    }
+    return null;
   }
 
   @SubscribeEvent
